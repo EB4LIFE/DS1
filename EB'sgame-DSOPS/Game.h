c5e868cf8fae7085c216f5612736b1bdd72ac050
EB4LIFE <105313463+EB4LIFE@users.noreply.github.com>
2024-06-16 02:02:16 -0400

    Update Game.h

--- EB'sgame-DSOPS/Game.h
+++ EB'sgame-DSOPS/Game.h
@@ -1,1 +1,28 @@
+#ifndef GAME_H
+#define GAME_H
+
+#include <vector>
+#include "Card.h"
+#include "Player.h"
+
+using namespace std;
+
+class Game {
+public:
+    vector<Card> deck;
+    vector<Card> discardPile;
+    Player user;
+    Player computer;
+
+    Game();
+
+    void createDeck();
+    void shuffleDeck();
+    void reshuffleDeck();
+    static void displayCard(const Card &card);
+    static void printOperationChoice(const Player &player);
+    void play();
+};
+
+#endif // GAME_H
 
